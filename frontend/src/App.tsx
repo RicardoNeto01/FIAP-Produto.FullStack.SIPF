3d0985330bd6b3cbda0f1dcca168018b474fd726
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-23 10:59:58 -0300

    Adicionando footer ao código

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Box } from "@mui/material"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 function App() {
 
   return (
     <Box>
       <Header title = "Gestão de produtos"/>
+
+      <Footer/>
     </Box>
   )
 }
