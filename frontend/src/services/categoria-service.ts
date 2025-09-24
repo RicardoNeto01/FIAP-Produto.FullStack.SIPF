5b59a7d65edad26670b47ca6c88bda2f39a32bd0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-24 09:08:56 -0300

    feat: Adicionando categoria service

---
+++ frontend/src/services/categoria-service.ts
@@ -0,0 +1,8 @@
+import type { CategoriaDTO } from "../models/categoria";
+import axios from "axios";
+import { BASE_URL } from "../utils/system";
+
+export async function findAll(): Promise<CategoriaDTO[]> {
+    const response = await axios.get(`${BASE_URL}/categorias`);
+    return response.data;
+}
